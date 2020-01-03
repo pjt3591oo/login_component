4052327e141c87db0c3cdd53aee3685daf565777
pjt3591oo <pjt3591oo@gmail.com>
2020-01-03 17:38:17 +0900

    Initial Commit

---
+++ LoginComponent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginComponent
+//
+//  Created by 박정태 on 2020/01/03.
+//  Copyright © 2020 JeongTae Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
